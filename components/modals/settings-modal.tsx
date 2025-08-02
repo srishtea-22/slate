955415502895148fa2e60fcd3406158269ff87e6
shristy <shristychaudhary4679@gmail.com>
2025-08-02 10:47:34 +0530

    Fix: dialog title

--- components/modals/settings-modal.tsx
+++ components/modals/settings-modal.tsx
@@ -18,10 +18,9 @@ export const SettingsModal = () => {
 
   return (
     <Dialog open={settings.isOpen} onOpenChange={settings.onClose}>
-      <DialogTitle>My Settings</DialogTitle>
       <DialogContent>
         <DialogHeader className="border-b pb-3">
-          <h2 className="text-lg font-medium">My settings</h2>
+          <DialogTitle>My Settings</DialogTitle>
         </DialogHeader>
         <div className="flex items-center justify-between">
           <div className="flex flex-col gap-y-1">
