7cec22a70d33630be46a9cc7308a888cdceccc99
shristy <shristychaudhary4679@gmail.com>
2025-08-02 21:48:56 +0530

    Feat: page title with emoji and editing

---
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Toolbar } from "@/app/(main)/_components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { use } from "react";
+
+interface DocumentPageProps {
+  params: Promise<{
+    documentId: Id<"documents">;
+  }>;
+}
+
+const DocumentIdPage = ({ params }: DocumentPageProps) => {
+  const { documentId } = use(params);
+  const document = useQuery(api.documents.getById, {
+    documentId,
+  });
+
+  if (document === undefined) return <div>Loading...</div>;
+  if (document === null) return null;
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
+};
+
+export default DocumentIdPage;
